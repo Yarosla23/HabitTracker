42b08ba13cbdd5b32a49f899e1ed2013b7207fbe
Aleksey Pryadko <aleksey.pryadko@jetruby.com>
2024-12-04 16:35:59 +0300

    Charts front

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,4 +1,6 @@
 class Profile < ApplicationRecord
   belongs_to :user
   mount_uploader :avatar, AvatarUploader
+
+  validates :username, presence: true, uniqueness: true, length: { minimum: 3, maximum: 50 }
 end
