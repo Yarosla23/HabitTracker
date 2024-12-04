a9ee47b83cc8d9347d5aba3af416c757c11573ee
Aleksey Pryadko <aleksey.pryadko@jetruby.com>
2024-12-04 14:42:16 +0300

    Charts data

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   before_action :authenticate_user!
   def index
+    @habits_count = current_user.habits_count
+    @days_registered = current_user.days_registered
   end
 end
